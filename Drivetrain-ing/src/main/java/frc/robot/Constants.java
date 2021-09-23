34066d9c604a1d1ef513f8570bc28e214ad2922d
Aniket Chakradeo <aniket@chakradeo.net>
2021-09-23 16:52:52 -0700

    added ports

--- Drivetrain-ing/src/main/java/frc/robot/Constants.java
+++ Drivetrain-ing/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int drive_lf = 24;
+    public static final int drive_lb = 13;
+    public static final int drive_rf = 23;
+    public static final int drive_rb = 15;
+}
