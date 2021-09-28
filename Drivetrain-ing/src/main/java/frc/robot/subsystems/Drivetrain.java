98f675efeb48cfc146494c1eb6bdb3384aabe4d2
SAnd353 <steven92127@gmail.com>
2021-09-28 16:31:17 -0700

    Pasted stuff from TeamOptix repo

--- Drivetrain-ing/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Drivetrain-ing/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,6 +8,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  private WPI_VictorSPX m_leftFrontMotor = new WPI_VictorSPX(Constants.CAN.drive_lf);
+  private WPI_TalonSRX m_leftBackMotor = new WPI_TalonSRX(Constants.CAN.drive_lb);
+  public SpeedControllerGroup m_leftMotors = new SpeedControllerGroup(m_leftFrontMotor, m_leftBackMotor);
+
+  private WPI_VictorSPX m_rightFrontMotor = new WPI_VictorSPX(Constants.CAN.drive_rf);
+  private WPI_TalonSRX m_rightBackMotor = new WPI_TalonSRX(Constants.CAN.drive_rb);
+  public SpeedControllerGroup m_rightMotors = new SpeedControllerGroup(m_rightFrontMotor, m_rightBackMotor);
+
+  public DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
   public Drivetrain() {}
     
   @Override
