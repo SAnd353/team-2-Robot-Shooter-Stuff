868572924492211a758c991b26b925a6c485e631
Dash Penning <dashpenning@gmail.com>
2021-10-21 16:26:38 -0700

    added stuff for shooter

--- Drivetrain-ing/src/main/java/frc/robot/subsystems/Shooter.java
+++ Drivetrain-ing/src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,6 +12,26 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class Shooter {
-    
+public class Shooter extends SubsystemBase {
+    private CANSparkMax m_shooterMotor = new CANSparkMax(10, MotorType.kBrushless);
+    private CANEncoder m_encoder = m_shooterMotor.getEncoder();
+    private CANPIDController m_controller = m_shooterMotor.getPIDController();
+
+    m_shooterMotor.restoreFactoryDefaults();
+
+    kP = 0.004; 
+    kI = 0;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000015; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
+  
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
 }
