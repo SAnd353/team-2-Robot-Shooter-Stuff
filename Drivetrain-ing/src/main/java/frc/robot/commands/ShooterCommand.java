08a3ed9a35f65b0c04ec89665fccf6c2e058b3a7
Dash Penning <dashpenning@gmail.com>
2021-10-21 16:14:02 -0700

    Added Shooter Subsystem

---
+++ Drivetrain-ing/src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+
+public class ShooterCommand extends CommandBase {
+
+  private final Drivetrain m_drive;
+  private final Timer m_timer = new Timer();
+
+
+  public ShooterCommand(Drivetrain drivetrain) {
+    m_drive = drivetrain;
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double time = m_timer.get();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.stopMotors();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
