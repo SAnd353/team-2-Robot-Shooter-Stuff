0923f2cbd0daa37ea1ca55afb754eb552016ca05
Dash Penning <dashpenning@gmail.com>
2021-10-21 16:17:52 -0700

    added imports to shooter

---
+++ Drivetrain-ing/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Shooter {
+    
+}
