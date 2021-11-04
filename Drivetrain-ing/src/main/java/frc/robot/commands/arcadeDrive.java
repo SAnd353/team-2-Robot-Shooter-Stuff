8601f52eda8e8cdb58ba60616cfc497ffa59702d
Dash Penning <dashpenning@gmail.com>
2021-11-04 15:44:42 -0700

    tells us distance

--- Drivetrain-ing/src/main/java/frc/robot/commands/arcadeDrive.java
+++ Drivetrain-ing/src/main/java/frc/robot/commands/arcadeDrive.java
@@ -35,5 +35,6 @@ public class arcadeDrive extends CommandBase {
   @Override
   public void execute() {
     m_drive.arcadeDrive(m_forward.getAsDouble(), m_rotation.getAsDouble());
+    System.out.prtintln(m_drive.distance());
   }
 }
