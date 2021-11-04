9b7267529a9c3e265a868c9ffe4819fe9514f2d1
Dash Penning <dashpenning@gmail.com>
2021-11-04 15:46:06 -0700

    liveshare

--- Drivetrain-ing/src/main/java/frc/robot/commands/DistPID.java
+++ Drivetrain-ing/src/main/java/frc/robot/commands/DistPID.java
@@ -5,13 +5,11 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class DistPID extends CommandBase {
 
   private final Drivetrain m_drive;
-  private final Timer m_timer = new Timer();
 
 
   public DistPID(Drivetrain drivetrain) {
@@ -22,14 +20,11 @@ public class DistPID extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_timer.reset();
-    m_timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double time = m_timer.get();
     if (m_drive.distance() < 1)
     m_drive.set(m_drive.getEncoderValue(), 2000);
   }
